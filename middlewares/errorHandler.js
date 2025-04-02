5ebf9013a3ebbe851015c96cd1e89b1f772c6ea7
NelsonHennessiAyodeji <nelsonhennessiayodeji@gmail.com>
2025-04-02 10:18:20 +0100

    Error Cleaning

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,13 +3,9 @@ const CustomErrorAPI = require("../errors/CustomErrorAPI");
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomErrorAPI) {
-    return res
-      .status(err.statusCode)
-      .json({ message: err.message, err: err.stack });
+    return res.status(err.statusCode).json({ message: err.message });
   }
-  res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: err.message, err: err.stack });
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
 };
 
 module.exports = errorHandler;
