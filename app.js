3fed21661daaadd475fc77a67ec4a9bd38ebe250
NelsonHennessiAyodeji <nelsonhennessiayodeji@gmail.com>
2025-04-02 01:10:34 +0100

    Clean-up, security and performance

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
+// Server Runner imports
 const express = require("express");
 const app = express();
+
+// Server security and performance imports
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const hpp = require("hpp");
+const compression = require("compression");
 const cookieParser = require("cookie-parser");
+
+// Router Imports
 const baseUrl = "api/v1/tryve-music";
 const authenticationRouter = require("./routers/authenticationRouter");
 const artistRouter = require("./routers/artistRouter");
 const eventRouter = require("./routers/eventRouter");
 const bookingRouter = require("./routers/bookingRouter");
+
+// Error Handler imports
 const notFoundError = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
 
-app.use(express.json());
+// Server scurity and performance middleware implementation
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+app.use(compression());
+app.use(cors());
+
+// Server internal middleware
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(express.json({ limit: "100kb" }));
+// app.use(express.static())
+
+// Server rate limiter
+app.use(
+  "/api",
+  rateLimit({
+    max: 200,
+    windowsMs: 60 * 60 * 1000, // 1 Hour
+    message: "Too many requests from this IP, please try again in an hour!",
+  })
+);
+
+// Server routers
 app.use(`/${baseUrl}/auth`, authenticationRouter);
 app.use(`/${baseUrl}/artists`, artistRouter);
 app.use(`/${baseUrl}/events`, eventRouter);
 app.use(`/${baseUrl}/bookings`, bookingRouter);
+
+// Server error handing implementations
 app.use(notFoundError);
 app.use(errorHandler);
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-require("express-async-errors");
+require("express-async-errors"); // This allows asynchronous functions to be used in route handlers without explicit error handling
 const app = require("./app");
 const connectDatabse = require("./database/database");
 
 const port = process.env.PORT || 3000;
 
-// Start method, if the databse has any faults the application WILL not run!
+// Start method, if the database has any faults the application WILL not run!
 app.listen(port, async () => {
   try {
     await connectDatabse(process.env.MONGO_URI);
